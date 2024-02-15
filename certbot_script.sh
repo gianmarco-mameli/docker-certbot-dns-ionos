6248cee193d6dbdbde1032c014731fc7b767e9f3
Gianmarco Mameli <gnammyx@gmail.com>
2024-02-15 22:20:15 +0100

    last mods

--- certbot_script.sh
+++ certbot_script.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
-# set -e
 
-env | grep -w  IONOS_DOMAINS > /dev/null
-if [ $? -eq 0 ]
-then
-   DOMAINS=$(echo "${IONOS_DOMAINS}" | tr "," " ")
-   for domain in ${DOMAINS}; do
-      DOMAINS_ARGS=${DOMAINS_ARGS}" -d $domain"
-   done
+if env | grep -w IONOS_DOMAINS >/dev/null; then
+	DOMAINS=$(echo "${IONOS_DOMAINS}" | tr "," " ")
+	for domain in ${DOMAINS}; do
+		DOMAINS_ARGS=${DOMAINS_ARGS}" -d ${domain}"
+	done
 else
-   echo "IONOS_DOMAINS not set"
-   exit 10
+	echo "IONOS_DOMAINS env not set"
+	exit 10
 fi
 
 /usr/local/bin/certbot certonly \
-                       --authenticator dns-ionos \
-                       --dns-ionos-credentials /etc/letsencrypt/.secrets/ionos.ini \
-                       --dns-ionos-propagation-seconds $IONOS_PROPAGATION \
-                       --non-interactive \
-                       --expand \
-                       --server $IONOS_SERVER \
-                       --agree-tos \
-                       --email $IONOS_EMAIL \
-                       --rsa-key-size 4096 $DOMAINS_ARGS
+	--authenticator dns-ionos \
+	--dns-ionos-credentials /etc/letsencrypt/.secrets/ionos.ini \
+	--dns-ionos-propagation-seconds "${IONOS_PROPAGATION}" \
+	--non-interactive \
+	--expand \
+	--server "${IONOS_SERVER}" \
+	--agree-tos \
+	--email "${IONOS_EMAIL}" \
+	--rsa-key-size 4096 "${DOMAINS_ARGS}"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-sudo echo "${IONOS_CRONTAB} /bin/sh /certbot_script.sh > /dev/stdout 2>&1" >> /etc/crontabs/root
 
-echo "docker-certbot-dns-ionos v${IONOS_VERSION} started
+echo "docker-certbot-dns-ionos v${IONOS_VERSION} started"
 
+echo "${IONOS_CRONTAB} /bin/sh /certbot_script.sh > /dev/stdout 2>&1" | sudo tee -a /etc/crontabs/root >/dev/null
+echo "Crontab entry ${IONOS_CRONTAB} added"
+
+echo "Starting crond"
 sudo /usr/sbin/crond -l 8 -f
