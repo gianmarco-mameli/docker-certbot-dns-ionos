32c815172f7050c44946a517278d926758f02ba3
Gianmarco Mameli <gnammyx@gmail.com>
2024-02-18 01:08:50 +0100

    test21345543

--- certbot_entry.sh
+++ certbot_entry.sh
@@ -2,10 +2,10 @@
 
 echo "docker-certbot-dns-ionos v${IONOS_VERSION} started"
 
-# sudo -E touch "/etc/crontabs/${USERNAME}"
 echo "${IONOS_CRONTAB} /bin/sh /certbot_script.sh > /dev/stdout 2>&1" | sudo -E tee -a /etc/crontabs/root >/dev/null
 echo "Crontab entry ${IONOS_CRONTAB} added"
 
-echo "Starting crond"
-sudo -E printenv | grep -vi "IONOS_" >> /etc/environment
+sudo touch /etc/environment
+echo "Starting crond with the following env variables"
+printenv | grep -i "IONOS_" | sudo -E tee -a /etc/environment
 /usr/sbin/crond -l 8 -f
