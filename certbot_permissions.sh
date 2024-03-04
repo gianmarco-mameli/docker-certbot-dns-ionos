8c579cf3b3b7a775e17270090af38a8701f709be
Gianmarco Mameli <gnammyx@gmail.com>
2024-03-04 12:04:14 +0100

    permissions

--- certbot_permissions.sh
+++ certbot_permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 chown -R "${USERNAME}":"${USERNAME}" "${CERTBOT_BASE_DIR}"
-chmod 644 "${CERTBOT_LIVE_DIR}"
-chmod 644 "${CERTBOT_ARCHIVE_DIR}"
+chmod 755 "${CERTBOT_LIVE_DIR}"
+chmod 755 "${CERTBOT_ARCHIVE_DIR}"
